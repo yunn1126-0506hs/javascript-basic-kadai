a5ae675be1fbe1dd9427f9c1cc6fa2cb8e380faa
yunn1126-0506hs <tko38ksmy38@gmail.com>
2024-11-10 14:43:49 +0900

    kadai_020ディレクトリの追加

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// btnというidを持つHTML要素を取得し、定数に代入
+
+const btn = document.getElementById('btn');
+let chengeTxt = document.getElementById('text');
+
+// HTML要素がクリックされた時にイベント処理を実行する
+
+btn.addEventListener('click', () => {
+　chengeTxt.textContent = 'ボタンをクリックしました'
+})
